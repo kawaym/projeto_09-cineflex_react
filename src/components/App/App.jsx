4ebf48dec3946d21a6ebaf1124a7a9f3e19fe889
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2021-12-14 21:52:18 -0300

    Integração a lista de filmes da API adicionada

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./reset.css";
 import "./App.css";
 import Pageframe from "../Pageframe/Pageframe";
 
 export default function App(){
+    const [posterFilmes, setPosterFilmes] = useState([]);
+    useEffect(() =>{
+        const promise = axios.get("https://mock-api.driven.com.br/api/v4/cineflex/movies");
+        promise.then((resposta) => setPosterFilmes(resposta.data));
+    }, []);
+    
+
     return (
         <>
             <Pageframe></Pageframe>
